9f075d9d2b64000dd9e37667debc25eb0d625bf8
Gavin Tranter <gavin_tranter@yahoo.co.uk>
2017-04-14 00:22:08 +0100

    Cleaning up tests

--- src/test/java/uk/tranter/itunes/playlister/ItunesPlaylisterTest.java
+++ src/test/java/uk/tranter/itunes/playlister/ItunesPlaylisterTest.java
@@ -7,8 +7,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 class ItunesPlaylisterTest {
 
     private final static List<String> track1 = Arrays.asList(
@@ -16,16 +14,12 @@ class ItunesPlaylisterTest {
             "<key>Track ID</key><integer>1000</integer>",
             "<key>Name</key><string>Song 1</string>",
             "<key>Artist</key><string>Artist 1</string>");
+
     private final static List<String> track2 = Arrays.asList(
             "<key>1000</key>",
             "<key>Track ID</key><integer>2000</integer>",
             "<key>Name</key><string>Song 2</string>",
             "<key>Artist</key><string>Artist 2</string>");
-
-    @Test
-    void myFirstTest() {
-        assertEquals(5, 3 + 2);
-    }
 
     @Test
     void shouldParseSingleTrackPlaylistXmlIntoPlaylist() {
@@ -39,7 +33,4 @@ class ItunesPlaylisterTest {
 
         Assertions.assertThat(playlist.getTracks()).contains(new Track("1000", "Artist 1", "Song 1"));
     }
-
-//            "<key>Track ID</key><integer>2000</integer>",
-//            "<key>Track ID</key><integer>1000</integer>",
 }
